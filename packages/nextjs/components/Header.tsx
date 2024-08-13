18399b5d8e70679ea20c4d55f4a391e81aa4645b
Jacob Homanics <homanicsjake@gmail.com>
2024-08-12 23:41:37 -0500

    replaced RainbowKit with WalletConnect

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -5,7 +5,9 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
-import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
+import {
+  FaucetButton, //, RainbowKitCustomConnectButton
+} from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
 type HeaderMenuLink = {
@@ -102,7 +104,8 @@ export const Header = () => {
         </ul>
       </div>
       <div className="navbar-end flex-grow mr-4">
-        <RainbowKitCustomConnectButton />
+        <w3m-button />
+        {/* <RainbowKitCustomConnectButton /> */}
         <FaucetButton />
       </div>
     </div>
--- packages/nextjs/services/web3/wagmiConfig.tsx
+++ packages/nextjs/services/web3/wagmiConfig.tsx
@@ -1,30 +1,61 @@
-import { wagmiConnectors } from "./wagmiConnectors";
-import { Chain, createClient, http } from "viem";
-import { hardhat, mainnet } from "viem/chains";
-import { createConfig } from "wagmi";
+// import { wagmiConnectors } from "./wagmiConnectors";
+import { defaultWagmiConfig } from "@web3modal/wagmi";
+// import { createWeb3Modal } from "@web3modal/wagmi";
+import {
+  Chain, //, createClient, http
+} from "viem";
+import {
+  //hardhat,
+  mainnet,
+} from "viem/chains";
+// import { createConfig } from "wagmi";
 import scaffoldConfig from "~~/scaffold.config";
-import { getAlchemyHttpUrl } from "~~/utils/scaffold-eth";
+
+// import { getAlchemyHttpUrl } from "~~/utils/scaffold-eth";
 
 const { targetNetworks } = scaffoldConfig;
 
 // We always want to have mainnet enabled (ENS resolution, ETH price, etc). But only once.
-export const enabledChains = targetNetworks.find((network: Chain) => network.id === 1)
+export const enabledChains = targetNetworks.find((network: Chain) => network.id == 1)
   ? targetNetworks
   : ([...targetNetworks, mainnet] as const);
 
-export const wagmiConfig = createConfig({
+// 1. Get projectId from https://cloud.walletconnect.com
+export const projectId = "ed460d9ee8baba1793b956223f5e0918";
+
+// 2. Create wagmiConfig
+export const metadata = {
+  name: "AppKit",
+  description: "AppKit Example",
+  url: "https://web3modal.com", // origin must match your domain & subdomain
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+export const wagmiConfig = defaultWagmiConfig({
   chains: enabledChains,
-  connectors: wagmiConnectors,
-  ssr: true,
-  client({ chain }) {
-    return createClient({
-      chain,
-      transport: http(getAlchemyHttpUrl(chain.id)),
-      ...(chain.id !== (hardhat as Chain).id
-        ? {
-            pollingInterval: scaffoldConfig.pollingInterval,
-          }
-        : {}),
-    });
+  projectId,
+  metadata,
+  auth: {
+    email: true, // default to true
+    socials: ["google", "x", "github", "discord", "apple", "facebook", "farcaster"],
+    showWallets: true, // default to true
+    walletFeatures: true, // default to true
   },
 });
+
+// export const wagmiConfig = createConfig({
+//   chains: enabledChains,
+//   connectors: wagmiConnectors,
+//   ssr: true,
+//   client({ chain }) {
+//     return createClient({
+//       chain,
+//       transport: http(getAlchemyHttpUrl(chain.id)),
+//       ...(chain.id !== (hardhat as Chain).id
+//         ? {
+//             pollingInterval: scaffoldConfig.pollingInterval,
+//           }
+//         : {}),
+//     });
+//   },
+// });
